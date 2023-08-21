aee8a47e52591f753abc0ea00b4a24cb36d5d382
Adholah96 <adholah96@gmail.com>
2023-08-21 16:39:53 +0300

    Changes to the solution

--- ruby_basics/1_data_types/exercises/numbers_exercises.rb
+++ ruby_basics/1_data_types/exercises/numbers_exercises.rb
@@ -15,6 +15,7 @@ end
 
 def divide(a, b)
   # return the result of dividing a by b
+  a / b
 end
 
 def remainder(a, b)
@@ -24,7 +25,7 @@ end
 
 def float_division(a, b)
   # return the result of dividing a by b as a float, rather than an integer
-  a.to_f / b
+  a / b.to_f
 end
 
 def string_to_number(string)
